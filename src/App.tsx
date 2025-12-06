e9d956628ccf7faf11edfed973a1a58206d22370
Tomasz Dubiel <dubieltomasz420@gmail.com>
2025-12-06 20:44:52 +0100

    Added searching in JSON for user inputted value

--- src/App.tsx
+++ src/App.tsx
@@ -4,37 +4,41 @@ import './App.css'
 function App() {
   return (
     <>
-      <Input/>
+      <Input />
       <table>
-        <tr>
-          <th colSpan={2}>Weapon</th>
-          <th>Type</th>
-          <th>Damage</th>
-          <th>Critical Boost</th>
-          <th>Scaling</th>
-          <th>Weight</th>
-          <th>Upgrade Material</th>
-        </tr>
-        <tr>
-          <td>Alfreds Futterkiste</td>
-          <td>Alfreds Futterkiste</td>
-          <td>Maria Anders</td>
-          <td>Alfreds Futterkiste</td>
-          <td>Alfreds Futterkiste</td>
-          <td>Maria Anders</td>
-          <td>Germany</td>
-          <td>Germany</td>
-        </tr>
-        <tr>
-          <td>Alfreds Futterkiste</td>
-          <td>Alfreds Futterkiste</td>
-          <td>Maria Anders</td>
-          <td>Alfreds Futterkiste</td>
-          <td>Alfreds Futterkiste</td>
-          <td>Maria Anders</td>
-          <td>Germany</td>
-          <td>Germany</td>
-        </tr>
+        <thead>
+          <tr>
+            <th colSpan={2}>Weapon</th>
+            <th>Type</th>
+            <th>Damage</th>
+            <th>Critical Boost</th>
+            <th>Scaling</th>
+            <th>Weight</th>
+            <th>Upgrade Material</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Alfreds Futterkiste</td>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Alfreds Futterkiste</td>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td>
+            <td>Germany</td>
+          </tr>
+          <tr>
+            <td>Alfreds Futterkiste</td>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Alfreds Futterkiste</td>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td>
+            <td>Germany</td>
+          </tr>
+        </tbody>
       </table>
     </>
   )
--- src/input.tsx
+++ src/input.tsx
@@ -1,10 +1,23 @@
+import {useState} from 'react'
+import JSONArray from '../public/csvjson(1).json'
+import JSONMap from '../public/csvjson.json'
+
 function input() {
+    function search(formData: FormData) {
+        const inputValue = formData.get('inputName')?.toString().toLowerCase();
+
+        if(inputValue) {
+            const matches = Object.keys(JSONMap).filter(key => key.toLowerCase().match(inputValue));
+            matches.forEach(match => console.log(match));
+        }
+    }
+
     return (
-        <form>
-            <input type="text" name="textInput"/>
+        <form action={search}>
+            <input type="text" name="inputName" />
             <button type="submit">Submit</button>
         </form>
-    );  
+    );
 };
 
 export default input;
